7d315dd0bb5e1bafb704c8d5c7794d268fcb5dec
Han Le <hanle.cs23@gmail.com>
2021-07-21 16:03:24 -0400

    Delete Preprocessing_queue

--- databaseconfig.py
+++ databaseconfig.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python3
 
-import preprocessing
-
 mysql = {
     "host": "localhost",
     "user": "root",
     "password": "your password",
     "database": "database",
 }
-preprocessing_queue = [
-    preprocessing.scale_and_center,
-    preprocessing.dot_reduction,
-    preprocessing.connect_lines,
-]
-use_anonymous = True
