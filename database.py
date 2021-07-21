41c35f9ee4141386e78d620e25dded25056c5741
Han Le <hanle.cs23@gmail.com>
2021-07-21 16:03:08 -0400

    Add checking existence into function add()

--- database.py
+++ database.py
@@ -18,8 +18,31 @@ def connect():
         return database
 
 
-def add():
-    pass
+def add(project_id, student_id, deploy_status, zone_id, creation_time, changelog):
+    connection = connect()
+    my_cursor = connection.cursor()
+    if not exist(project_id):
+        query = "INSERT INTO student (project_id, student_id, deploy_status, zone_id, creation_date, changelog) VALUES (%s, %s, %d, ('SELECT zone_id FROM zone WHERE zone_name = %s'), %s, %s)"
+        value = (project_id, student_id, deploy_status,
+                 zone_id, creation_time, changelog)
+        my_cursor.execute(query, value)
+        connection.commit()
+    else:
+        query = "UPDATE student SET deploy_status = %d, zone_id = ('SELECT zone_id FROM zone WHERE zone_name = %s'), creation_time = %s, changelog = %s WHERE project_id = %s"
+        value = (deploy_status, zone_id, creation_time, changelog, project_id)
+        my_cursor.execute(query, value)
+        connection.commit()
+
+
+def exist(project_id):
+    connection = connect()
+    my_cursor = connection.cursor()
+    check_username = my_cursor.execute(
+        'SELECT project_id FROM student WHERE project_id = %(project_id)s', (project_id))
+    if check_username != 0:
+        return False
+    else:
+        return True
 
 
 def close(database):
