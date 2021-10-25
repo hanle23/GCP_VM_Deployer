b027f661ddd90a2a1c2862ea5c6f4fd6f72e8811
Quang Hieu Le <hanlecs@yorku.ca>
2021-10-25 09:59:07 -0400

    Organize files into folders

--- tests/deploy_instance_test.py
+++ tests/deploy_instance_test.py
@@ -1,8 +1,5 @@
-import access
+# from GCP-Auto-Deploy.v1 import access, deploy_instance, student_data
 import unittest
-from deploy_instance import *
-import student_data as data
-
 
 compute, service = access.authorize()
 assert compute != None
@@ -11,31 +8,31 @@ assert service != None
 
 class DeployTestCase(unittest.TestCase):
     def test_list_projects(self):
-        projects = list_projects(service)
+        projects = deploy_instance.list_projects(service)
         self.assertIsInstance(projects, list)
         self.assertIsNotNone(projects)
 
     def test_choose_zone(self):
-        zone = choose_zone(compute)
+        zone = deploy_instance.choose_zone(compute)
         self.assertTrue(zone.startswith("us-east"))
 
     def test_namevalid(self):
         name = "bda-123456789"
-        self.assertTrue(namevalid(name))
+        self.assertTrue(deploy_instance.namevalid(name))
 
     def test_get_student_txt(self):
-        student_list = data.get_student_id_txt("test_file.txt")
+        student_list = student_data.data.get_student_id_txt("test_file.txt")
         self.assertTrue(student_list)
         self.assertIsNotNone(student_list)
 
     def test_create_instance(self):
-        zone = choose_zone(compute)
+        zone = deploy_instance.choose_zone(compute)
         projects = ["testing-project-322415", "testing-project-322416"]
         for project in projects:
-            create_instance(compute, project, zone)
-            names = get_instance_names(compute, project)
+            deploy_instance.create_instance(compute, project, zone)
+            names = deploy_instance.get_instance_names(compute, project)
             self.assertIn("bda-db-1", names)
-            delete_instance(compute, project, zone)
+            deploy_instance.delete_instance(compute, project, zone)
 
 
 if __name__ == '__main__':
